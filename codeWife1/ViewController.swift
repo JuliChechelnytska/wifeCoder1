8de182d3653311a7e795606285681f1f51a81e79
Juli <Juli1>
2020-02-29 23:11:09 +0300

    start

---
+++ codeWife1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  codeWife1
+//
+//  Created by Julia on 19.02.2020.
+//  Copyright © 2020 Julia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
